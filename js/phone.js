226e07debc47cabdad581ecbd4b06735ca883578
Mohsin-Hossain <mohsinshawon18@gmail.com>
2022-03-02 14:29:00 +0600

    card add

--- js/phone.js
+++ js/phone.js
@@ -9,12 +9,31 @@ const searchPhone = () =>{
 
 
 // load phone Data 
-const url ='https://openapi.programming-hero.com/api/phones?search=iphone'
+const url =`https://openapi.programming-hero.com/api/phones?search=${searchText}`
  fetch (url)
  .then(res =>res.json())
- .then(data => displayPhone(data))
+ .then(data => displayPhone(data.data))
+ 
 }
 
-const displayPhone = Phones =>{
-    console.log(Phones)
+const displayPhone = phones=>{
+    const searchResults =document.getElementById('search-results');
+    phones.forEach(phone => {
+        console.log(phone);
+        const div= document.createElement('div')
+        div.classList.add('col')
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h2 class="card-title">${phone.phone_name}</h2>
+              <h5>${phone.brand}</h5>
+            </div>
+            
+          </div>
+        `;
+        searchResults.appendChild(div)
+    })
 }
+    
+
